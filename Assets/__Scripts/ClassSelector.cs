7f22b7c87330343a228798006d72bb23f483abcb
ZachRaposo <45317768+ZachRaposo@users.noreply.github.com>
2020-04-01 01:37:00 -0400

    Prevents spamming of abilities

--- Assets/__Scripts/ClassSelector.cs
+++ Assets/__Scripts/ClassSelector.cs
@@ -16,6 +16,7 @@ public class ClassSelector : MonoBehaviour
     private float tmp;
     public GameObject[] classItems2;
     public GameObject frame;
+    private bool wait = true;
 
     public void Dropdown_Index(int index)
     {
@@ -58,15 +59,17 @@ public class ClassSelector : MonoBehaviour
 
     void Update()//This is where all the powerups are, simply press B with the respective class
     {
+        
         if (classnum == 0)//if the user selected the Crusador class and presses B then it makes the player grow and then shrink back to normal size
         {
             frame.SetActive(true);
             classItems2[0].SetActive(true);
-            if (classnum==0&&Input.GetKeyDown(KeyCode.B))
+            if (classnum==0&&Input.GetKeyDown(KeyCode.B)&&(wait==true))
             {
                 Cooldown = Stats.Cooldown(Cooldown);
                 if (Cooldown > 0)
                 {
+                    wait = false;
                     player1.transform.localScale += new Vector3(1, 1, 1);
                     Debug.Log(Cooldown+2);
                     print("Super Strength Activated!");
@@ -74,6 +77,7 @@ public class ClassSelector : MonoBehaviour
                 }
                 else
                 {
+                    wait = false;
                     player1.transform.localScale += new Vector3(1, 1, 1);
                     Debug.Log(Cooldown+5);
                     print("Super Strength Activated!");
@@ -89,11 +93,12 @@ public class ClassSelector : MonoBehaviour
             frame.SetActive(true);
             classItems2[1].SetActive(true);
 
-            if ((classnum == 1) && (Input.GetKeyDown(KeyCode.B)))//if the user selected the Mage class and presses B then it makes the user invisible for a few seconds
+            if ((classnum == 1) && (Input.GetKeyDown(KeyCode.B))&&(wait==true))//if the user selected the Mage class and presses B then it makes the user invisible for a few seconds
             {
                 Cooldown = Stats.Cooldown(Cooldown);
                 if (Cooldown > 0)
                 {
+                    wait = false;
                     player1.transform.Rotate(0f, 90f, 0f);
                     Debug.Log(Cooldown + 2);
                     print("Invisibility Cloak Activated!");
@@ -102,6 +107,7 @@ public class ClassSelector : MonoBehaviour
                 }
                 else
                 {
+                    wait = false;
                     player1.transform.Rotate(0f, 90f, 0f);
                     Debug.Log(Cooldown + 5);
                     print("Invisibility Cloak Activated!");
@@ -114,11 +120,12 @@ public class ClassSelector : MonoBehaviour
             frame.SetActive(true);
             classItems2[2].SetActive(true);
 
-            if ((classnum == 2) && (Input.GetKeyDown(KeyCode.B)))//if the user selected the Archer class and presses B then the user swiftly side steps enemies
+            if ((classnum == 2) && (Input.GetKeyDown(KeyCode.B)) && (wait == true))//if the user selected the Archer class and presses B then the user swiftly side steps enemies
             {
                 Cooldown = Stats.Cooldown(Cooldown);
                 if (Cooldown == 5)
                 {
+                    wait = false;
                     tmp = 3;
                     distance = tmp+ (Cooldown - (Cooldown - 1));
                     player1.transform.Translate(distance, 0, 0);
@@ -128,6 +135,7 @@ public class ClassSelector : MonoBehaviour
                 }
                 if (Cooldown == 7)
                 {
+                    wait = false;
                     tmp = 4;
                     distance = tmp + (Cooldown - (Cooldown - 1));
                     player1.transform.Translate(distance, 0, 0);
@@ -137,6 +145,7 @@ public class ClassSelector : MonoBehaviour
                 }
                 if (Cooldown == 9)
                 {
+                    wait = false;
                     tmp = 5;
                     distance = tmp + (Cooldown - (Cooldown - 1));
                     player1.transform.Translate(distance, 0, 0);
@@ -146,6 +155,7 @@ public class ClassSelector : MonoBehaviour
                 }
                 if (Cooldown == 11)
                 {
+                    wait = false;
                     tmp = 6;
                     distance = tmp + (Cooldown - (Cooldown - 1));
                     player1.transform.Translate(distance, 0, 0);
@@ -155,6 +165,7 @@ public class ClassSelector : MonoBehaviour
                 }
                 else if(Cooldown==0)
                 {
+                    wait = false;
                     distance = 3;
                     player1.transform.Translate(distance, 0, 0);
                     Debug.Log(distance);
@@ -163,7 +174,7 @@ public class ClassSelector : MonoBehaviour
                 }
                 else if(Cooldown>11)
                 {
-                    
+                    wait = false;
                     player1.transform.Translate(distance, 0, 0);
                     Debug.Log(distance);
                     print("Side Stepped!");
@@ -177,21 +188,20 @@ public class ClassSelector : MonoBehaviour
     {
         player1.transform.Rotate(0f, 270f, 0f);
         print("Invisibility Cloak has worn off!");
+        wait = true;
     }
     void Shrink()//shrinks the grown player
     {
         player1.transform.localScale -= new Vector3(1, 1, 1);
         print("Super Strength Deactivated!");
-      
+        wait = true;
+
     }
     void SideStep()//preforms the sidestep in the opposite direction
     {
         player1.transform.Translate(-distance, 0, 0);
+        wait = true;
     }
 
-    void GetCoolDown(float cooldown)
-    {
-
-    }
 }
 
