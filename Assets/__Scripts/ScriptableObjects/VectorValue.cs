76c74f390e88c81746c6db068ea94bfbaa84f0b3
mginham <45317731+mginham@users.noreply.github.com>
2020-04-01 19:34:47 -0400

    SceneTransitionP1
    Goes from house interior to outer world

---
+++ Assets/__Scripts/ScriptableObjects/VectorValue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu] // Allows us to create this as an object with a right click
+public class VectorValue : ScriptableObject
+{
+    public Vector2 initialValue;
+}
